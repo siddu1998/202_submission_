e1b56cef42266e332a24c494727f89a0196cd8a7
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2022-02-09 16:50:19 -0800

    flask

---
+++ app.py
@@ -0,0 +1,20 @@
+from transformers import pipeline, set_seed
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/getStory")
+def getStory():
+    
+    data = request.json['data']
+    print(data)
+    generator = pipeline('text-generation', model='gpt2')
+    set_seed(42)
+    story=generator(data, max_length=100, num_return_sequences=1)
+    print(story)
+    return {'story':story}
+
+if __name__ == '__main__':
+   app.run()
+   app.run(debug=True)
