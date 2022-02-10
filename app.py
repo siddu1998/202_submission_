f1579669494ea4e53687c8804cf3a8000f865e26
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2022-02-10 12:30:30 -0800

    ceaser

--- app.py
+++ app.py
@@ -4,17 +4,32 @@ from flask import request
 
 app = Flask(__name__)
 
+
+def encrypt(text,s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text      
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+        return result
+
+
 @app.route("/getStory")
 def getStory():
     
-    data = request.json['data']
+    data  = request.json['data']
+    seed = request.json['seed'] 
+
     # print(data)
     # generator = pipeline('text-generation', model='gpt2')
     # set_seed(42)
     # story=generator(data, max_length=100, num_return_sequences=1)
-    # summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    # story = summarizer(data, max_length=130, min_length=30, do_sample=False)
-    story = data.lower()
+
+    story = encrypt(data,seed)
     return {'story':story}
 
 if __name__ == '__main__':
